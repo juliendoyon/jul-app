87e7c031222fa8db222128118449d571fbda7ff1
juldoy <julien.doyon@berger-levrault.ca>
2019-04-16 09:34:41 -0400

    Test 1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello pas world');
+ res.send('hello pas world 2');
 });
 //listen to port 8080 by default
 app.listen(process.env.PORT || 8080);
